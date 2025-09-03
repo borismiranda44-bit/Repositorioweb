f6bfc306628d1f57cca0e0148e924e13326c3236
Alan <Borismiranda44@gmail.com>
2025-09-02 21:39:09 -0400

    Primer commit

---
+++ ejer_uno.py
@@ -0,0 +1,70 @@
+#print("mi nombre es boris")
+#print("vivo en la ciudad de el alto")
+
+
+
+#a = 15
+#print(type(a))
+#b = 3.14159
+#print(type(b))
+#c = "Hola mundo"
+#print(type(c))
+#d = True
+#print(type(d))
+#e = None
+#print(type(e))
+
+
+#print("El tipo de este valor es:", type(a))
+
+
+
+
+
+#print("Convierte la cadena \"12345\" a un entero y luego a un float.")
+#print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
+
+#cadena_uno = "12345"
+#entero = int (cadena_uno)
+#floer = float(entero)
+#print(floer)
+
+#dolar_dos = 3.99
+
+#print (int(dolar_dos))
+
+
+#print(type(dolar_dos))
+
+
+
+
+#print("\nEjercicio 4: Variables")
+#print("Crea variables para tu nombre, edad y altura.")
+#print("Usa f-strings para imprimir una presentación.")
+
+# "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
+
+
+#nombre = "me llamo boris alan"
+# edad = 23
+#altura = float(1.80)
+
+#print(f"hola me llamo {nombre} y tengo {edad} ,mido{altura} metros")
+
+
+#numero_p = 3.1416
+
+#numero_dos = round(numero_p)
+#print(numero_dos)
+
+#division = (numero_dos / 2)
+#print(division)
+
+
+print("mi nombre es juanito y tengo:", 234)
+
+#resultado = int(round(3.1416) / 2)
+#print("Valor de PI (aproximado):", 3.1416)
+#print("PI redondeado:", round(3.1416))
+#print("División entera de PI redondeado entre 2:", resultado)
